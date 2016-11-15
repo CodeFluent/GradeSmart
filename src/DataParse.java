3c960246857cc5f8664b4a8e669bc59f24937d34
Wasfi Momen <wasfimomenwim@gmail.com>
2016-11-15 16:19:38 -0500

    updated code with exceptions

--- src/DataParse.java
+++ src/DataParse.java
@@ -17,23 +17,26 @@ import java.io.PrintWriter;
  *  DataParse is a Java class that scans the html file given,
  *  extracts the content of certain HTML elements, and then
  *  concats the strings into a CSV file.
+ *
+ *  DataParse is NOT executed in the main application. It is a utility class meant to grab and parse data for input
+ *  only once and would not be provided to the end user.
  */
 
 public class DataParse {
 
-    /**
-     *
-     *
-     * @return returns a String to become a line in the CSV file.
-     */
-    public static void CSVString () {
+
+    public static void FileOutput () {
         try {
 
             Scanner console = new Scanner(System.in);
             System.out.println("Enter the html file where data is stored: ");
             String file = console.nextLine();
             Document document = Jsoup.parse(new File(file), "utf-8");
-            System.out.println(document.title());
+
+            String line = CSVString(document); // returns the line to be put into the CSV file
+            System.out.println(line);
+
+
 
         } catch(IOException e) {
             e.printStackTrace();
@@ -41,10 +44,19 @@ public class DataParse {
 
     }
 
+    public static String CSVString (Document document) {
+
+        String title = document.title();
+
+        return title;
+
+
+    }
+
 
 
     public static void main(String [] args) {
-        CSVString();
+        FileOutput();
 
     }
 
